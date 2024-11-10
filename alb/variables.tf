7dd1b90e408eb991846c5e3adb5a3650dd449bc8
Ernest <apcitestgithub@gmail.com>
2024-11-09 21:20:02 -0500

    first commit

---
+++ alb/variables.tf
@@ -0,0 +1,24 @@
+variable "vpc_id" {
+  type = string
+}
+
+variable "frontend_subnet_az_1a_id" {
+  type = string
+}
+
+variable "frontend_subnet_az_1b_id" {
+  type = string
+}
+
+variable "tags" {
+  type = map(string)
+}
+
+
+variable "certificate_arn" {
+  type = string
+}
+
+variable "ssl_policy" {
+  type = string
+}
